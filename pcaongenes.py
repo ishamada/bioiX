9b1047b035877239db0be089369b4991893d9611
balqees-mansour <87857777+balqees-mansour@users.noreply.github.com>
2022-10-31 21:39:16 +0200

    Create pcaongenes.py

--- pcaongenes.py
+++ pcaongenes.py
@@ -8,21 +8,13 @@ panel_filename = "phase1_integrated_calls.20101123.ALL.panel"
 
 genotypes = []
 samples = []
-
-
-
-
-
-
-
-
-if counter % 4943 == 0:
-            print(counter)
-            print(f'{round(100 * counter / 494328)}%')
-        # if counter >= 10000:
-        #     break
-
-with open(panel_filename) as panel_file:
-    labels = {}  # {sample_id: population_code}
-    for line in panel_file:
-        line = line.strip().split('\t')
+variant_ids = []
+with VariantFile(vcf_filename) as vcf_reader:
+    counter = 0
+    for record in vcf_reader:
+        counter += 1
+        if counter % 100 == 0:
+            alleles = [record.samples[x].allele_indices for x in record.samples]
+            samples = [sample for sample in record.samples]
+            genotypes.append(alleles)
+            variant_ids.append(record.id)
