8c9435834c0cede2e8105530e7b919f566e8bbb9
Islam Hamada <islamsalah@gmail.com>
2022-10-31 21:33:40 +0200

    Update pcaongenes.py

--- pcaongenes.py
+++ pcaongenes.py
@@ -2,3 +2,9 @@ from pysam import VariantFile
 import numpy as np
 from sklearn import decomposition
 import pandas as pd
+
+vcf_filename = "ALL.chr22.phase1_release_v3.20101123.snps_indels_svs.genotypes.vcf.gz"
+panel_filename = "phase1_integrated_calls.20101123.ALL.panel"
+
+genotypes = []
+samples = []
